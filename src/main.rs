30ad8093e0732590614b82561534764df63f4708
Dan Enman <enmand@gmail.com>
2026-09-21 11:02:32 -0300

    chore: add cloud for output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod cli;
 mod commands;
+mod github;
 mod targets;
 
 use anyhow::Result;
@@ -9,6 +10,14 @@ use cli::{Cli, Command};
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
+
+    clout::init()
+        .with_verbose(4)
+        .with_quiet(false)
+        .with_silent(false)
+        .with_use_color(clout::UseColor::Auto)
+        .done()
+        .expect("clout failed to init");
 
     match cli.command {
         Command::Assess { target, policy } => commands::assess(&target, policy.as_deref()),
